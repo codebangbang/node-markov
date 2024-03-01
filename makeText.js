ac90608cfe4499c3cf45cf6d2bc2feff9476daee
ryanfair <ryfair@gmail.com>
2024-02-29 19:22:27 -0500

    Step 0 and 1 complete

---
+++ makeText.js
@@ -0,0 +1,22 @@
+/** Command-line tool to generate Markov text. */
+
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process");
+
+function generateText(text) {
+  let mm = new markov.MarkovMachine(text);
+  console.log(mm.makeText());
+}
+
+function makeText(path) {
+  fs.readFile(path, "utf8", function cb(err, data) {
+    if (err) {
+      console.error(`Cannot read file: ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      generateText(data);
+    }
+  });
+}
